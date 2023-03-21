e8d4748af1b27b3abd0cddffb97070a867848946
jacquemota <jaque_mota15@hotmail.com>
2023-03-21 13:31:36 -0300

    first commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 3;
+        System.out.println("porcariaaaaa\n" + (a+b));*/
+        gato gato = new gato();
+        System.out.println(gato);
+    }
+}
